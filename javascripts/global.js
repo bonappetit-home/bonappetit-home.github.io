15d8f32627456e9a8851302a094e2a5a1ceaac10
Fercholpz <fercholpz88@gmail.com>
2015-07-31 15:32:47 -0500

    input behaviour

--- javascripts/global.js
+++ javascripts/global.js
@@ -1,5 +1,23 @@
 jQuery(document).ready(function($) {
-    $('#youtube1').mediaelementplayer();
-    $('#youtube2').mediaelementplayer();
+  $('#youtube1').mediaelementplayer();
+  $('#youtube2').mediaelementplayer();
+
+  $(".js-button-input").on("click", function(){
+  	$(this).addClass("btn-reserve--width");
+		setTimeout(function(){ 
+			$(".js-button-input").fadeOut();
+			$(".js-box--input").fadeIn();
+			$(".js-input-email").focus();
+		}, 300);
+
+
+  });
+
+
+  $(".js-do-reservet").on("click", function(){
+  	
+  });
+  
 
 });
+
