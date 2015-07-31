fbc79551e11b09b65c208f888dc6614d0916f654
Fercholpz <fercholpz88@gmail.com>
2015-07-31 16:24:45 -0500

    added email validation

--- javascripts/global.js
+++ javascripts/global.js
@@ -14,16 +14,27 @@ jQuery(document).ready(function($) {
   });
 
 
-  $(".js-do-reservet").on("click", function(){
-		Parse.initialize("1xzWcLBL9xXTP4s3XpdNYChaLP5MjZ7QPDNUiuDe", "co9aMd2V4V7DXeewbm9o27DRxES0NQup4iDWiWR1");
-		  Parse.Cloud.run('sendReservation', { email: 'Tests' }, {
+  $(".js-do-reserve").on("click", function(){
+  	var emailVal = $(".js-input-email").val();
+  	if(validateEmail(emailVal)){
+  		Parse.initialize("1xzWcLBL9xXTP4s3XpdNYChaLP5MjZ7QPDNUiuDe", "co9aMd2V4V7DXeewbm9o27DRxES0NQup4iDWiWR1");
+		  Parse.Cloud.run('sendReservation', { email: emailVal }, {
 			  success: function(result) {
 			    alert(result);
 			  },
 			  error: function(error) {
 			    alert(error);
 			  }
-  	});
+  		});
+  	}else{
+  		alert("Uh oh, something went wrong, please try again");
+  	}
+		
 	});
+
+	function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+	}
 });
 
